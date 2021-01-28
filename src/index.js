f20f2fe5e8ffe2063f096f1d3485541d53ad5609
montecamo <merrychristek@gmail.com>
2021-01-28 21:10:14 +0300

    add three.js squares renderer

--- src/index.js
+++ src/index.js
@@ -1,44 +1,91 @@
+// Option 1: Import the entire three.js core library.
+import * as THREE from "three";
 
-import { makeWave, makeSea, shakeSea } from './sea';
-import { makeStepper } from './stepper';
+import { makeWave, makeSea, shakeSea } from "./sea";
+import { makeStepper } from "./stepper";
+import { makeSquare, calcTileDepth } from "./square";
+import { makeTile } from "./tile";
 
-const wave = makeWave(x => Math.sin(x * 50) * 200);
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+window.three = THREE;
+
+// const cube1 = new THREE.Mesh(new THREE.BoxGeometry( 1, 1, 1 ), new THREE.MeshPhongMaterial( {color: 0x00ff00} ));
+// scene.add(cube1);
+
+const color = 0xffffff;
+const intensity = 1;
+const light = new THREE.DirectionalLight(color, intensity);
+light.position.set(-1, 2, 4);
+scene.add(light);
+
+camera.position.z = 150;
+camera.position.y = 0;
+camera.position.x = 20;
+
+const size = 1;
+
+const spacing = 1.3;
+
+var grid = new THREE.Object3D();
+const boxes = [];
+
+const SQUARE = 61;
+
+for (let i = 0; i < SQUARE * SQUARE; i++) {
+  boxes.push(
+    new THREE.Mesh(
+      new THREE.BoxBufferGeometry(size, size, size),
+      new THREE.MeshPhongMaterial({ color: 0x00ff00 })
+    )
+  );
+}
+
+scene.add(grid);
+
+grid.rotation.x += 2;
+
+const wave = makeWave((x) => Math.sin(x * 100) * 10);
 const stepper = makeStepper(0.001);
-let sea = makeSea(230);
+let sea = makeSea(SQUARE);
 
-const container = document.getElementById('wave');
+const square = makeSquare(SQUARE * SQUARE);
+const calc = calcTileDepth(square);
 
-const column = (height) => {
-  const el = document.createElement('div')
-  el.classList.add('column');
+const animate = function () {
+  setTimeout(() => {
+    requestAnimationFrame(animate);
+  }, 30);
 
-  el.style.height = `${Math.abs(height)}px`;
-
-  if (height < 0) {
-    el.style.transform = `translateY(-100%)`;
-  }
-
-  return el;
-}
-
-
-const render = (sea) => {
-  container.innerHTML = '';
-  sea.forEach((s) => {
-    container.appendChild(column(s));
-  });
-}
-
-const start = () => {
   sea = shakeSea(sea)(wave(stepper()));
 
-  render(sea);
+  for (var i = 0; i < SQUARE; i++) {
+    for (var j = 0; j < SQUARE; j++) {
+      const tile = makeTile(j, i);
+      const depth = calc(tile);
 
-  setTimeout(() => {
-    window.requestAnimationFrame(start);
-  }, 50);
-}
+      const box = boxes[i * SQUARE + j];
 
-start();
+      box.position.x = j * spacing;
+      box.position.y = i * spacing;
 
+      box.position.z = Math.abs(sea[depth]);
 
+      grid.add(box);
+    }
+  }
+
+  renderer.render(scene, camera);
+};
+
+animate();
