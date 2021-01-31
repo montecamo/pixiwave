aeaabad68cc2c1ad0a069a0e3c8a49984a41282f
montecamo <merrychristek@gmail.com>
2021-01-31 15:40:12 +0300

    support point z

--- src/shapes/point.ts
+++ src/shapes/point.ts
@@ -1,9 +1,11 @@
 export type Point = {
   x: number;
   y: number;
+  z: number;
 };
 
-export const makePoint = (x: number, y: number) => ({ x, y });
+export const makePoint = (x: number, y: number, z: number = 0) => ({ x, y, z });
 
 export const getPointX = (point: Point) => point.x;
 export const getPointY = (point: Point) => point.y;
+export const getPointZ = (point: Point) => point.z;
