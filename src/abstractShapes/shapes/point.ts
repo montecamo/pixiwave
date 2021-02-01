91421b6d7b3232559f2e030af05c93a1cd68c541
montecamo <merrychristek@gmail.com>
2021-02-02 00:34:55 +0300

    add points distance calculator

--- src/abstractShapes/shapes/point.ts
+++ src/abstractShapes/shapes/point.ts
@@ -1,3 +1,5 @@
+import { square } from "../../utils";
+
 export type Point = {
   x: number;
   y: number;
@@ -9,3 +11,11 @@ export const makePoint = (x: number, y: number, z: number = 0) => ({ x, y, z });
 export const getPointX = (point: Point) => point.x;
 export const getPointY = (point: Point) => point.y;
 export const getPointZ = (point: Point) => point.z;
+
+export function distanceBetweenPoints(a: Point, b: Point): number {
+  return Math.abs(
+    Math.sqrt(
+      square(getPointX(a) - getPointX(b)) + square(getPointY(a) - getPointY(b))
+    )
+  );
+}
