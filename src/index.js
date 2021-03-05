62245a8ff7796c6a18abe085838771c1a045a5e3
montecamo <merrychristek@gmail.com>
2021-03-05 11:27:35 +0300

    feat: add wave spawner, using three raycaster

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ import {
   makeBasicWaveShape,
   makeWaveFunction,
 } from "./abstractShapes";
-import { installControls } from "./controls";
+import { installControls, installMap } from "./controls";
 
 import { makeRenderer } from "./renderer";
 
 const options = {
   amplitude: 10,
   frequency: 0.1,
-  size: 3,
+  size: 30,
   speed: 0.01,
 };
 
@@ -27,18 +27,54 @@ const shape = makeBasicShape(
 );
 const wave = makeBasicWaveShape(
   centerPoint,
+  //makePoint(-options.size / 2, -options.size / 2),
   { type: "circle" },
   { type: "infinite", speed: options.speed, func: waveFunction }
 );
+const wave2 = makeBasicWaveShape(
+  makePoint(options.size / 2, options.size / 2),
+  { type: "circle" },
+  { type: "pulse", speed: options.speed, func: waveFunction }
+);
+const wave3 = makeBasicWaveShape(
+  makePoint(options.size / 2, -options.size / 2),
+  { type: "circle" },
+  { type: "pulse", speed: options.speed, func: waveFunction }
+);
+const wave4 = makeBasicWaveShape(
+  makePoint(-options.size / 2, options.size / 2),
+  { type: "circle" },
+  { type: "pulse", speed: options.speed, func: waveFunction }
+);
 
 const coreRenderer = makeRenderer(shape);
-coreRenderer.addWave(wave);
+//coreRenderer.addWave(wave);
+//coreRenderer.addWave(wave2);
+//coreRenderer.addWave(wave3);
+//coreRenderer.addWave(wave4);
 
 const threeRenderer = new ThreeRenderer(document.body);
 threeRenderer.init();
 
 const stats = installStats(document.getElementById("stats"));
 installControls(options);
+
+window.addEventListener("click", () => {
+  const hoveredCoordinates = threeRenderer.getHoveredBoxCoordinates();
+
+  if (hoveredCoordinates) {
+    coreRenderer.addWave(
+      makeBasicWaveShape(
+        makePoint(
+          hoveredCoordinates.x - options.size / 2,
+          hoveredCoordinates.y - options.size / 2
+        ),
+        { type: "circle" },
+        { type: "pulse", speed: options.speed, func: waveFunction }
+      )
+    );
+  }
+});
 
 function loop() {
   stats.begin();
