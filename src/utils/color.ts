4fbfcef43e4a0b2ceb8a9937b96409af4bc40440
montecamo <merrychristek@gmail.com>
2021-03-06 13:20:15 +0300

    feat(color): add aggressive caching

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,10 +1,23 @@
 import chroma from "chroma-js";
+import { makeHashTable } from "./hashtable";
 
 export type Color = string;
+
+const hashTable = makeHashTable((colors) => colors.join(""));
 
 export function randomColor(): Color {
   return chroma.random().css();
 }
 export function mixColors(colors: Array<Color>): Color {
-  return chroma.average(colors).css();
+  const cached = hashTable.get(colors);
+
+  if (cached) {
+    return cached;
+  }
+
+  const result = chroma.average(colors).css();
+
+  hashTable.set(colors, result);
+
+  return result;
 }
