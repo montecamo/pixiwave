6a171749e0d6e4d760ecd527c534844923730087
montecamo <merrychristek@gmail.com>
2021-03-07 17:48:32 +0300

    chore: change palette

--- src/three-renderer/constants.ts
+++ src/three-renderer/constants.ts
@@ -1,5 +1,5 @@
 export const AMBIENT_LIGHT_COLOR = 0xffffff;
 export const SPOT_LIGHT_COLOR = 0xffffff;
-export const BACKGROUND_COLOR = 0x0dceda;
-export const BOX_COLOR = 0xebfffa;
+export const BACKGROUND_COLOR = 0x10ddc2;
+export const BOX_COLOR = 0xf57170;
 export const ACTIVE_BOX_COLOR = 0x882999;
